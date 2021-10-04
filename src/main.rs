a1e5dab0d4a87cddab4059a513dea5f77d411875
harshsha5 <harshbitsp@gmail.com>
2021-10-03 22:32:06 -0700

    Add error handling and color

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+use colored::*;
 
 fn main() {
-    println!("Guess the number?");
-    
-    let mut guess = String::new();
-    io::stdin()
+
+    let secret_number = rand::thread_rng().gen_range(1,10);
+
+    loop {
+
+        println!("Guess the number?");
+        
+        let mut guess = String::new();
+        io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
-
-    println!("You guessed: {}",guess);
+        
+        let guess: u32 = guess.trim().parse().expect("Please type a number");    
+        
+        println!("You guessed: {}",guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}","Your number was too small".red()),
+            Ordering::Equal => {
+                println!("{}","Your number was an exact match".green());
+                break;
+            },
+            Ordering::Greater => println!("{}","Your number was too big".red()),
+        }
+    }
+    println!("The secret number was: {}",secret_number); 
 }
